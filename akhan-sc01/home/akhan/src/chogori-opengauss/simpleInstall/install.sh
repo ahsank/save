66bc8671398d6d7e002c3e4c6fbc604cd0f5e157
Ahsan Khan <akhan@futurewei.com>
2022-09-27 12:43:51 -0700

    Update simpleInstall/install.sh

--- akhan-sc01/home/akhan/src/chogori-opengauss/simpleInstall/install.sh
+++ akhan-sc01/home/akhan/src/chogori-opengauss/simpleInstall/install.sh
@@ -212,7 +212,7 @@ function set_environment() {
 
 function single_install() {
     info "[step 6]: init datanode"
-    gdb --args gs_initdb -w $password -D $app/data/single_node --nodename "sgnode" --locale="en_US.UTF-8"
+    ${GDB} gs_initdb -w $password -d -D $app/data/single_node --nodename "sgnode" --locale="en_US.UTF-8"
     if [ X$port != X$default_port  ]
     then
         sed -i "/^#port =/c\port = $port" $app/data/single_node/postgresql.conf
